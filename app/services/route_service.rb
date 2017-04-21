ecf23b85376afa11e7391c16e00d035b0076250a
Rodrigo Monteiro <rodrigo.monteiro@vagas.com.br>
2017-04-21 19:25:30 -0300

    Add RouteService shameless green

--- app/services/route_service.rb
+++ app/services/route_service.rb
@@ -11,6 +11,34 @@ class RouteService
     Distance.where(origin: place)
   end
 
+  def find_shortest
+    distances = distances_from(origin)
+
+    distances.each do |distance|
+      path = Path.new
+      path.distances << distance
+      @paths << path
+    end
+
+    dijkstra_recursivity
+  end
+
+  def dijkstra_recursivity
+    path = shortest_path
+
+    return path if path.end == destination
+
+    distances = distances_from(path.end)
+
+    distances.each do |distance|
+      @paths << path.create_new(distance)
+    end
+
+    remove_path(path)
+
+    dijkstra_recursivity
+  end
+
   def remove_path(path)
     @paths.delete(path)
   end
--- spec/services/route_service_spec.rb
+++ spec/services/route_service_spec.rb
@@ -19,15 +19,6 @@ describe RouteService do
     end
   end
 
-  describe '.scan' do
-    subject { instance.scan }
-
-    let(:instance) { described_class.new('A', 'C') }
-
-    context 'when has only one option' do
-    end
-  end
-
   describe '.distances_from' do
     subject { described_class.new('A', 'B').distances_from('A') }
 
@@ -61,6 +52,47 @@ describe RouteService do
 
       it 'returns two' do
         expect(subject.count).to eq(2)
+      end
+    end
+  end
+
+  describe '.find_shortest' do
+    subject { instance.find_shortest }
+
+    let(:instance) { described_class.new('A', 'C') }
+
+    context 'when needs only one step' do
+      context 'and only one distance' do
+        before do
+          Distance.create({origin: 'A', destination: 'C', kilometers: 5})
+        end
+
+        it 'returns path' do
+          expect(subject.distance).to eq(5)
+        end
+      end
+
+      context 'and two distances' do
+        before do
+          Distance.create({origin: 'A', destination: 'C', kilometers: 5})
+          Distance.create({origin: 'A', destination: 'B', kilometers: 10})
+        end
+
+        it 'returns path with less kilometers' do
+          expect(subject.distance).to eq(5)
+        end
+      end
+    end
+
+    context 'when needs more than one step' do
+      before do
+        Distance.create({origin: 'A', destination: 'B', kilometers: 10})
+        Distance.create({origin: 'B', destination: 'C', kilometers: 15})
+        Distance.create({origin: 'A', destination: 'C', kilometers: 30})
+      end
+
+      it 'returns path with less kilometers' do
+        expect(subject.distance).to eq(25)
       end
     end
   end
